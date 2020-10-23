32847bec8a427ea69000985d22d88feef0ca08ad
sp756897 <akshaysp2090@gmail.com>
2020-10-23 13:07:24 +0530

    Updates

---
+++ precache-manifest.0af404bed94bc3d7ba91455619e39adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bc60da9e8e4a2f38ae75856561cdb2",
+    "url": "/Vircom-Team-Web/index.html"
+  },
+  {
+    "revision": "c1b0026eb0a72debcc1e",
+    "url": "/Vircom-Team-Web/static/css/main.6c1bf35b.chunk.css"
+  },
+  {
+    "revision": "990272eecf30828297d3",
+    "url": "/Vircom-Team-Web/static/js/2.c5d4b155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vircom-Team-Web/static/js/2.c5d4b155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b0026eb0a72debcc1e",
+    "url": "/Vircom-Team-Web/static/js/main.e27bb961.chunk.js"
+  },
+  {
+    "revision": "8406a39e2496f9fd6884",
+    "url": "/Vircom-Team-Web/static/js/runtime-main.8da4adbd.js"
+  }
+]);
